8d00628ffb27cb1a58680814b39aeff7ba923cd3
JenniferKamily <jenniferparreira2017@gmail.com>
2023-11-30 19:26:28 -0300

    ..

---
+++ editarPerfil.php
@@ -0,0 +1,22 @@
+<?php
+    require("conexao.php");
+    session_start();
+    $matricula = $_SESSION['ra'];
+    $query= "SELECT*FROM usuarios where matricula = '$matricula';";
+    $result = mysqli_query($conexao,$query);
+
+    if (!$result) {
+        die("Erro na consulta ao banco de dados: " . mysqli_error($conexao));
+    }
+    $row = mysqli_num_rows($result);
+    if ($row > 0) {
+        $valor = mysqli_fetch_assoc($result);
+    }
+      echo $_POST['nick'];
+      $query = "UPDATE usuarios SET apelido = '".$_POST['nick']."' WHERE nome = '".$valor['NOME']."';";
+      if(mysqli_query($conexao, $query)) {
+        echo "Alterações feitas com sucesso";
+      }else{
+        echo "Falha ao conectar";
+      }
+?>
--- pgusuario.php
+++ pgusuario.php
@@ -14,6 +14,17 @@
         $valor = mysqli_fetch_assoc($result);
     }
 
+    function update(){
+      global $conexao;
+      global $valor;
+      $query = "UPDATE usuarios SET APELIDO = '".$_POST['nick']."' WHERE NOME = '".$valor['NOME']."';";
+      if(mysqli_query($conexao, $query)) {
+        echo "Alterações feitas com sucesso";
+      }else{
+        echo "Falha ao conectar";
+      }
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -68,14 +79,39 @@
                     <!-- Imagem do Usu&aacute;rio -->
                     <img src="9d5bc2be-7e88-4ddb-a01e-d8f53d7ef0c9.jpeg" alt="Imagem do Usu&aacute;rio" class="img-fluid rounded-circle mb-3" style="max-width: 150px;"><!-- Dados do Usu&aacute;rio -->
                     <p><span id="nomeUser"><strong>Nome: </strong><?php echo $valor['NOME']?></span></p>
+                    <p id="apelido"><strong>Nickname: </strong><?php echo $valor['APELIDO']?></p>
                     <p id="faculdadeUser"></p>
                     <p id="raUser"><strong>Matrícula: </strong><?php echo $valor['MATRICULA']?></p>
                     <p id="resUser"></p>
-                   <!-- Bot&atilde;o Editar --><button class="btn btn-primary">Editar</button>
+                   <!-- <button class="btn btn-primary" method='POST' action='pgusuario.php' name='editar'>Editar</button> -->
+                   <!-- <input name="editar" type="submit" method="POST" onclick="pgusuario.php" value="Editar"/> -->
                 </div>
             </div>
         </div>
     </div>
 </div>
-<script src="pgusuario.js"></script>
+<div class="row">
+        <!-- Informa&ccedil;&otilde;es do Usu&aacute;rio -->
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Editar Informa&ccedil;&otilde;es do Usu&aacute;rio</h4>
+                </div>
+                <div class="card-body">
+                    <!-- <img src="9d5bc2be-7e88-4ddb-a01e-d8f53d7ef0c9.jpeg" alt="Imagem do Usu&aacute;rio" class="img-fluid rounded-circle mb-3" style="max-width: 150px;">Dados do Usu&aacute;rio -->
+                  <form method="POST" action="editarPerfil.php">
+                    <div class="form-group">
+                      <div class="input-group mb-3 input-btn" >
+                        <span class="input-group-text" id="inputGroup-sizing-default">Nome</span>
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"placeholder="Nickname" name="nick" id="nome">
+                      </div> 
+                    </div>
+                      <button class="btn btn-primary" name='editar'>Editar</button>
+                  </form>
+                   <!-- <input name="editar" type="submit" method="POST" onclick="pgusuario.php" value="Editar"/> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script></body></html>
